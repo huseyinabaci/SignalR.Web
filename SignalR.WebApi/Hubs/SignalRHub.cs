b4c21a3ea40dace25ee075b4b5ebe6d047dad7ba
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2024-05-20 21:31:28 +0300

    Add project files.

---
+++ SignalR.WebApi/Hubs/SignalRHub.cs
@@ -0,0 +1,34 @@
+﻿using Dapper.Application.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.WebApi.Hubs
+{
+    public class SignalRHub : Hub
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public SignalRHub(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task SendStatistic()
+        {
+            var data = await unitOfWork.Products.GetAllAsync();
+            await Clients.All.SendAsync("ReceiveStatistic", data.Count());
+        }
+
+        public static int clientCount { get; set; } = 0;
+        public override async Task OnConnectedAsync()
+        {
+            clientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            clientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", clientCount);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ SignalR.WebApi/Program.cs
@@ -0,0 +1,49 @@
+using Dapper.Application.Interfaces;
+using Dapper.Infrastructure.Repository;
+using SignalR.WebApi.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddSignalR();
+
+builder.Services.AddControllers();
+
+// Add services from Infrastructure
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
+
+app.Run();
